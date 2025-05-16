014b9593f95502f047b9dec592c6b05b0f925418
jasper <zhaojinxinfdu@gmail.com>
2025-05-16 10:39:31 +0800

    add template content

--- template.h
+++ template.h
@@ -3,6 +3,7 @@
 
 #include "common.h"
 
+// traditional way to realize the template pattern
 class AbstractClass {
 public:
     void TemplateMethod() const {
@@ -57,5 +58,65 @@ protected:
     }
 };
 
+// using template to reduce virtual table derivatives
+// sum up common functions into func: _TemplateMethod
+// 
+class TPLAbstractClass {
+public:
+    virtual ~TPLAbstractClass() = default;
+    virtual void TemplateMethod() const = 0;
+
+protected:
+    template <typename T>
+    static void _TemplateMethod(T & obj);
+
+protected:
+    virtual void RequireOperation1() const {
+        std::cout << "BaseTPL1 Implemention operation1\n";
+    }
+    virtual void RequireOperation2() const {
+        std::cout << "BaseTPL2 Implemention operation2\n";
+    }
+};
+
+template <typename T>
+void TPLAbstractClass::_TemplateMethod(T & obj) {
+    this->BaseOperation1();
+        this->RequireOperation1();
+        this->BaseOperation2();
+        this->Hook1();
+        this->RequireOperation2();
+        this->BaseOperation3();
+        this->Hook2();
+}
+
+class SubTPLClass : public TPLAbstractClass {
+public:
+    void TemplateMethod() const override;
+
+protected:
+    void BaseOperation1() const {
+        std::cout << "Abstraction base operation1" << std::endl;
+    }
+    void BaseOperation2() const {
+        std::cout << "Abstraction base operation2\n";
+    }
+    void BaseOperation3() const {
+        std::cout << "Abstraction base operation3\n";
+    }
+    void Hook1() const {}
+    void Hook2() const {}
+
+    void RequireOperation1() const override {
+        std::cout << "SubTPL1 Implemention operation1\n";
+    }
+    void RequireOperation2() const override {
+        std::cout << "SubTPL2 Implemention operation2\n";
+    }
+};
+
+void SubTPLClass::TemplateMethod() const {
+    _TemplateMethod(*this);
+}
 
 #endif
